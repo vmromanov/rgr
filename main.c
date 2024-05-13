51b92f7622f8b82b0c05483a5ff9f70cd23de0fd
vmromanov <145842293+vmromanov@users.noreply.github.com>
2024-05-13 13:50:12 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,95 @@
-#include <stdio.h>
-#include "rgr.h"
+﻿#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main()
+int power(int base, int exponent) {
+    if (exponent < 0) return -1;
+    int result = 1;
+    if (exponent == 0)
+        return 1;
+    for (int i = 0; i < exponent; ++i)
+        result = result * base;
+    return result;
+}
+
+void ShellSort(int n, float* mass, int amount, int* step)
 {
-	printf("enter file name");
-	char name[1000]; scanf("%c",name);
-	result* res = rgr(name);
-	
+   
+
+  
+    return;
 }
 
 
+int main()
+{
+	float arr[10000] = { 0 };
+	
+    int x, y;
+    int counter = 0;
+    for (int i = 0; (x = power(2, i)) < 10000; ++i)
+        for (int j = 0; (y = power(3, j)) < 10000; ++j)
+            if (x * y < 10000)
+                counter++;
+
+    int* arr23 = malloc(sizeof(int) * counter);
+    
+    int u = 0;
+    for (int i = 0; (x = power(2, i)) < 10000; ++i)
+        for (int j = 0; (y = power(3, j)) < 10000; ++j)
+            if (x * y < 10000 && u<counter)
+            {
+                arr23[u] = x * y;
+                ++u;
+            }
+
+    for (int i = 0; i < counter - 1; i++) {
+        
+        for (int j = 0; j < counter - i - 1; j++) {
+            if (arr23[j] < arr23[j + 1]) { 
+
+                int temp = arr23[j]; 
+                arr23[j] = arr23[j + 1]; 
+                arr23[j + 1] = temp; 
+            }
+        }
+    }
+
+
+    for (int i = 0; i < counter; ++i)
+        printf(arr23[i]);
+   /* ShellSort(10000, arr, counter, arr23);*/
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+}
